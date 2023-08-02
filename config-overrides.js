2b625bbe36770a7347c158e7c93b7faffba9d03d
Sadamune <zwiebelshi@gmail.com>
2023-08-02 23:32:04 +0900

    common icon paths

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,25 @@
-const path = require('path');
-const { override, addWebpackAlias, addWebpackModuleRule } = require('customize-cra');
+const path = require("path");
+const {
+  override,
+  addWebpackAlias,
+  addWebpackModuleRule,
+} = require("customize-cra");
 
 module.exports = override(
-    addWebpackAlias({
-        '@': path.resolve(__dirname, 'src'),
-    }),
-    addWebpackModuleRule({
-        test: /\.scss$/,
-        use: [
-            {
-                loader: 'resolve-url-loader',
-                options: { sourceMap: true },
-            },
-            {
-                loader: 'sass-loader',
-                options: { sourceMap: true },
-            },
-        ],
-    })
+  addWebpackAlias({
+    "@": path.resolve(__dirname, "src"),
+  }),
+  addWebpackModuleRule({
+    test: /\.scss$/,
+    use: [
+      {
+        loader: "resolve-url-loader",
+        options: { sourceMap: true },
+      },
+      {
+        loader: "sass-loader",
+        options: { sourceMap: true },
+      },
+    ],
+  }),
 );
